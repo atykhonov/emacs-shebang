3d7a13d6cbb5f72cecb4146601285c8e230fd2a0
Andrey Tykhonov <atykhonov@gmail.com>
2014-02-06 00:28:51 +0200

    * shebang.el: Docstrings updated

--- shebang.el
+++ shebang.el
@@ -31,6 +31,7 @@
 ;;
 ;; (require 'shebang)
 ;; (global-set-key "\C-c b i" 'shebang-insert)
+;; (global-set-key "\C-c b I" 'shebang-insert-at-point)
 ;;
 ;;  Change the key bindings to your liking.
 ;;
@@ -39,24 +40,25 @@
 
 ;;
 ;; `shebang-insert' queries for a command and inserts shebang line for
-;; it.
+;; it. Shebang inserts at the beginning of buffer.
 ;;
 ;; For example an execution of such command:
 ;;
 ;; M-x shebang-insert RET python RET
 ;;
-;; will insert at point shebang line like:
+;; will insert at the beginning of buffer shebang line like:
 ;;
 ;; #!/usr/bin/python
 ;;
 ;; With a `C-u' frefix `shebang-insert' queries for a command and
-;; insert shebang line for `env' and a command.
+;; insert shebang line for `env' and a command. `env' is a programm
+;; from GNU Coreutils (sample location: /usr/bin/env).
 ;;
 ;; As example an execution of such command:
 ;;
 ;; C-u M-x shebang-insert RET perl RET
 ;;
-;; will insert at point shebang line like:
+;; will insert at the beginning of buffer shebang line like:
 ;; 
 ;; #!/usr/bin/env perl
 ;;
